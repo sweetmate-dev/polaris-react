c3ddf227975982f7c8c3393231d3527e69d2c91a
Ben Scott <ben.scott@shopify.com>
2020-09-22 14:38:34 -0700

    Buttons can't have role=alert

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -332,7 +332,6 @@ export function Button({
         aria-controls={ariaControls}
         aria-expanded={ariaExpanded}
         aria-pressed={ariaPressedStatus}
-        role={loading ? 'alert' : undefined}
         aria-busy={loading ? true : undefined}
       >
         {content}
